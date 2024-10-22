3a298ebd2faec46381359f10303bdb8018c10172
jsommers <jsommers@colgate.edu>
2024-10-22 10:51:07 -0400

    update specs, cucumber

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,10 @@
 class Student < ApplicationRecord
   def name
-    "#{first.capitalize} #{last.capitalize}"
     # NOT @first.capitalize
+    "#{first.capitalize} #{last.capitalize}"
   end
 
-  def self.name_search(string)
-    first, last = string.split(/ +/)
-    Student.where("first like ?", "%#{first}%").or(Student.where("last like ?", "%#{last}%"))
+  def self.name_search(name)
+    Student.where('first LIKE ?', "%#{name}%").or(Student.where('last LIKE ?', "%#{name}%"))
   end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe Student, type: :model do
       expect(s.name).to eq("Abc Def")
     end
   end
+
+  context "#name_search" do
+    before(:each) do 
+      @a = Student.create!(first: "apple", last: "potatohead")
+      @b = Student.create!(first: "kiwi", last: "melonhead")
+    end
+
+    it "should correctly find student by name (one match)" do
+      expect(Student.name_search('kiwi')).to match_array([@b])
+    end
+
+    it "should correctly find student by name (multiple match)" do
+      expect(Student.name_search('head')).to match_array([@a, @b])
+    end
+
+    it "should correctly find student by name (no match)" do
+      expect(Student.name_search('stuff')).to match_array([])
+    end
+  end
 end
